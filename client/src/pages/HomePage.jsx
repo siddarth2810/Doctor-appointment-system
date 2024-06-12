0b79d8f6eb0868605f1f6de8f168158901f64573
siddarth2810 <siddarthg0910@gmail.com>
2024-06-12 21:53:29 +0530

    add register and fix bootstrap

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 
     return (
         <>
             <h2>homepage</h2>
+            <Link to="/register">Register</Link>
         </>
     );
 }
