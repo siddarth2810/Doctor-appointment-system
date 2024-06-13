dd46d7b82dd7c1b94b9a3886af8d2f5f8a537795
siddarth2810 <siddarthg0910@gmail.com>
2024-06-13 10:07:28 +0530

    fix mongo in yaml

--- server.js
+++ server.js
@@ -16,15 +16,12 @@ connectDB();
 // middlewares
 app.use(express.json()); // to reduce parsing errors
 app.use(morgan('dev'));
-app.use(cors({
-  origin: 'https://doctor-appointment-system-i8f8-cay8h9myn-siddarth2810s-projects.vercel.app/', // Specify your client URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+app.use(cors()); // enable CORS
+
 // routes
 app.use("/api/v1/user", require('./routes/userRoutes'));
 
 app.listen(8082, () => {
-    console.log(`Server Running in ${process.env.NODE_ENV} Mode on port 8082`);
+    console.log(`Server Running in ${process.env.NODE_ENV} Mode`);
 });
 
